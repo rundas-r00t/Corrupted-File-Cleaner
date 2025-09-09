9af7cda5d57dc87834aba912123bc21de37c1216
rundas-r00t <rundas_r00t@proton.me>
2025-09-09 12:19:27 -0600

    Update Strings-Cleaner.ps1
    updated to include streamReader for speed because strings.txt will probably be over 100GB

--- Strings-Cleaner.ps1
+++ Strings-Cleaner.ps1
@@ -4,19 +4,32 @@
 $InputFile = "c:\strings.txt"
 $OutputFile = "c:\strings_clean.txt"
 
+# Clear output if exists
+if (Test-Path $OutputFile) { Clear-Content $OutputFile }
+
 # Regex to match:
 # 1. email only: something@something.something
 # 2. email:password (allow anything after colon)
 $EmailPattern = '^[\w\.-]+@[\w\.-]+\.\w+(:.*)?$'
 
-# Clear output if exists
-if (Test-Path $OutputFile) { Clear-Content $OutputFile }
+# Buffer settings
+$BufferSize = 50000
+$Buffer = New-Object System.Collections.Generic.List[string]
 
-# Process line by line
-Get-Content $InputFile -ReadCount 1000 | ForEach-Object {
-    foreach ($line in $_) {
-        if ($line -match $EmailPattern) {
-            Add-Content -Path $OutputFile -Value $line
+# Stream the file line by line
+$reader = [System.IO.StreamReader]::new($InputFile)
+while (($line = $reader.ReadLine()) -ne $null) {
+    if ($line -match $EmailPattern) {
+        $Buffer.Add($line)
+        if ($Buffer.Count -ge $BufferSize) {
+            [System.IO.File]::AppendAllLines($OutputFile, $Buffer)
+            $Buffer.Clear()
         }
     }
+}
+$reader.Close()
+
+# Final flush
+if ($Buffer.Count -gt 0) {
+    [System.IO.File]::AppendAllLines($OutputFile, $Buffer)
 }
